8865aa341b7ba0e5b8d7c9547013fdf44c9a0f9b
Carlos Eduardo Dante <contato.carloscdante@gmail.com>
2024-01-26 02:37:18 -0300

    removed node modules from repo

--- node_modules/@libsql/client/lib-esm/web.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Config, Client } from "./api.js";
-import type { ExpandedConfig } from "./config.js";
-export * from "./api.js";
-export declare function createClient(config: Config): Client;
-/** @private */
-export declare function _createClient(config: ExpandedConfig): Client;
